6f17b0b02d24c436264003280eb76a6168802c39
Armingul <svillarr@ucm.es>
2020-11-30 12:02:15 +0100

    js creados

---
+++ config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+      host: "localhost",// Ordenador que ejecuta el SGBD
+      user: "root",   // Usuario que accede a la BD
+      password: "",  // Contraseña con la que se accede a la BD
+      database: "404"  // Nombre de la base de datos
+}

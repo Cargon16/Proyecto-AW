fe79c2100b5b53b4e72ff27a26f7ba52885b5ce4
Armingul <svillarr@ucm.es>
2020-12-02 14:35:42 +0100

    busqueda.html y paginaPreguntasSinResponder acabadas

---
+++ vistas/paginaPreguntasSinResponder.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    require("headers.html");
+    ?>
+</head>
+<body>
+    <header>
+    <?php
+        require("headerLogged.html");
+    ?> 
+    </header>
+    
+    <?php
+        require("busqueda.html");
+    ?> 
+
+    <?php
+        require("preguntas.html");
+    ?> 
+    
+</body>
+</html>

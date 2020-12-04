0341543d9aff822f3873b811acbe63cc1497bb9a
Armingul <svillarr@ucm.es>
2020-12-04 17:14:39 +0100

    controladorUsuario

--- Routers/controladorUsuario.js
+++ Routers/controladorUsuario.js
@@ -48,3 +48,81 @@ function profile(request, response) {
         }
     });
 }
+
+function logout(request, response){
+    response.status(200);
+    response.session.destroy();
+    response.redirect("/login");
+}
+
+function procesarLogin(request, response){
+    daoUsuario.isUserCorrect(request.locals.emailUsuario, request.locals.password, function(err, existe){
+        if(err){
+            response.status(500);
+            console.log("procesarLogin_post" + err);\
+            next(err);
+        }else{
+            response.status(200);
+            if(existe){
+                request.session.usuarioActual = request.locals.emailUsuario;
+            }else{
+                response.render("preocesarLogin",{
+                    errorMsg: "Direccion de correo y/o password no validos"
+                })
+            }
+        }
+    })
+}
+
+function usuarioRegistrado(request, response){
+
+    var usuario = function(request){
+        if(request.locals.emailUsuario == "" || request.locals.password == "" || request.locals.passwordConfirm == "" || request.locals.nombreMostrar){
+            return false;
+
+        }else{
+            var imagen = null;
+
+            //aqui iria lo de la imagen
+            /**
+             * if (request.file) {
+                imageName = request.file.filename;
+                 }
+             */
+
+             var user = {
+                 email : request.locals.emailUsuario,
+                 password : request.locals.password,
+                 passwordConfirm : request.locals.passwordConfirm,
+                 nombreMostrar : request.locals.nombreMostrar
+             };
+
+             return user;
+
+        }
+    }
+    if(usuario == false){
+        response.render("registro", {
+            msg: "Revisa los campos obligatorios(*)"
+        });
+    }else{
+        daoUsuario.insertUsuario(usuario, function(err, insertado){
+            if(err){
+                response.status(500);
+                console.log(err + "post_usuarioRegistrado");
+                next(err);
+            }else{
+                response.status(200);
+                if(insertado){
+                    response.render("registro",{
+                        msg: "Usuario creado."
+                    })
+                }else{
+                    response.render("registro",{
+                        msg: "Error al crear usuario"
+                    })
+                }
+            }
+        })
+    }
+}

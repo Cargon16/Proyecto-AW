707c43b29dc61bab9f1d52f98b59416e1dcfd673
Cargon16 <cargon16@ucm.es>
2021-01-14 17:14:50 +0100

    últimos cambios

--- 404.sql
+++ 404.sql
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 DROP DATABASE IF EXISTS `404`;
 CREATE DATABASE `404` ;
+USE `404`;
 
 CREATE TABLE `medallas` (
   `ID_Medalla` int(100) NOT NULL,
@@ -234,3 +235,51 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+INSERT INTO `usuarios` (`Correo`, `Contraseña`, `Nombre`, `Reputacion`, `FechaAlta`, `FotoPerfil`) VALUES ("nico@404.es", 12345678, "Nico", 1, "2021-01-13", "nico.png");
+INSERT INTO `usuarios` (`Correo`, `Contraseña`, `Nombre`, `Reputacion`, `FechaAlta`, `FotoPerfil`) VALUES ("roberto@404.es", 12345678, "Roberto", 1, "2021-01-13", "roberto.png");
+INSERT INTO `usuarios` (`Correo`, `Contraseña`, `Nombre`, `Reputacion`, `FechaAlta`, `FotoPerfil`) VALUES ("sfg@404.es", 12345678, "SFG", 1, "2021-01-13", "sfg.png");
+INSERT INTO `usuarios` (`Correo`, `Contraseña`, `Nombre`, `Reputacion`, `FechaAlta`, `FotoPerfil`) VALUES ("marta@404.es", 12345678, "Marta", 1, "2021-01-13", "marta.png");
+INSERT INTO `usuarios` (`Correo`, `Contraseña`, `Nombre`, `Reputacion`, `FechaAlta`, `FotoPerfil`) VALUES ("lucas@404.es", 12345678, "Lucas", 1, "2021-01-13", "kuroko.png");
+INSERT INTO `usuarios` (`Correo`, `Contraseña`, `Nombre`, `Reputacion`, `FechaAlta`, `FotoPerfil`) VALUES ("emy@404.es", 12345678, "Emy", 1, "2021-01-13", "amy.png");
+
+
+INSERT INTO preguntas (Titulo, Cuerpo, ID_Usuario, Fecha, Reputacion, Votos, Visitas) VALUES ("¿Cual es la diferencia entre position: relative, position: absolute y position: fixed?","Sé que estas propiedades de CSS sirven para posicionar un elemento dentro de la página. Sé que estas propiedades de CSS sirven para posicionar un elemento dentro de la página.", "nico@404.es", "2021-01-13", 0, 0,0);
+INSERT INTO tag (ID_Pregunta, tag) VALUES (1,"css");
+INSERT INTO tag (ID_Pregunta, tag) VALUES (1,"css3");
+INSERT INTO respuestas (Cuerpo, ID_Usuario, ID_Pregunta, Fecha, Reputacion, Votos) VALUES ("La propiedad position sirve para posicionar un elemento dentro de la página. Sin embargo, dependiendo de cual sea la propiedad que usemos, el elemento tomará una referencia u otra para posicionarse respecto a ella.
+Los posibles valores que puede adoptar la propiedad position son: static | relative | absolute | fixed | inherit | initial.", "lucas@404.es", 1, "2021-01-13", 0, 0);
+
+
+INSERT INTO preguntas (Titulo, Cuerpo, ID_Usuario, Fecha, Reputacion, Votos, Visitas) VALUES ("¿Cómo funciona exactamente nth-child?","No acabo de comprender muy bien que hace exactamente y qué usos prácticos puede tener.", "roberto@404.es", "2021-01-13", 0, 0,0);
+INSERT INTO tag (ID_Pregunta, tag) VALUES (2,"css");
+INSERT INTO tag (ID_Pregunta, tag) VALUES (2,"html");
+INSERT INTO respuestas (Cuerpo, ID_Usuario, ID_Pregunta, Fecha, Reputacion, Votos) VALUES ("La pseudoclase :nth-child() selecciona los hermanos que cumplan cierta condición definida en la fórmula an + b. a y b deben ser números enteros, n es un contador. El grupo an representa un ciclo, cada cuantos elementos se repite; b indica desde donde empezamos a contar.", "emy@404.es", 2, "2021-01-13", 0, 0);
+
+
+INSERT INTO preguntas (Titulo, Cuerpo, ID_Usuario, Fecha, Reputacion, Votos, Visitas) VALUES ("Diferencias entre == y === (comparaciones en JavaScript)","
+Siempre he visto que en JavaScript hay:
+
+asignaciones =
+comparaciones == y ===
+Creo entender que == hace algo parecido a comparar el valor de la variable y el === también compara el tipo (como un equals de java).
+", "sfg@404.es", "2021-01-13", 0, 0,0);
+INSERT INTO tag (ID_Pregunta, tag) VALUES (3,"JavaScript");
+
+
+INSERT INTO preguntas (Titulo, Cuerpo, ID_Usuario, Fecha, Reputacion, Votos, Visitas) VALUES ("Problema con asincronismo en Node","
+Soy nueva en Node... Tengo una modulo que conecta a una BD de postgres por medio de pg-node. En eso no tengo problemas. Mi problema es que al llamar a ese modulo, desde otro modulo, y despues querer usar los datos que salieron de la BD me dice undefined... Estoy casi seguro que es porque la conexion a la BD devuelve una promesa, y los datos no estan disponibles al momento de usarlos.", "marta@404.es", "2021-01-13", 0, 0, 0);
+INSERT INTO tag (ID_Pregunta, tag) VALUES (4,"nodejs");
+
+
+INSERT INTO preguntas (Titulo, Cuerpo, ID_Usuario, Fecha, Reputacion, Votos, Visitas) VALUES ("¿Qué es la inyección SQL y cómo puedo evitarla?","
+He encontrado bastantes preguntas en StackOverflow sobre programas o formularios web que guardan información en una base de datos (especialmente en PHP y MySQL) y que contienen graves problemas de seguridad relacionados principalmente con la inyección SQL.
+
+Normalmente dejo un comentario y/o un enlace a una referencia externa, pero un comentario no da mucho espacio para mucho y sería positivo que hubiera una referencia interna en SOes sobre el tema así que decidí escribir esta pregunta.", "lucas@404.es", "2021-01-13", 0, 0, 0);
+INSERT INTO tag (ID_Pregunta, tag) VALUES (5,"mysql");
+INSERT INTO tag (ID_Pregunta, tag) VALUES (5,"sql");
+
+
+
+
+
